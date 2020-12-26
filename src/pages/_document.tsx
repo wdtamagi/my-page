14dfacb9cf4a03a85d87e6499394874a106e430c
Willian Dias Tamagi <will.tamagi@gmail.com>
2020-12-26 17:06:22 -0300

    Update main next files

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,11 @@ class MyDocument extends Document {
           <link rel="icon" sizes="192x192" href="/img/icons/favicon-192.png" />
           <link rel="apple-touch-icon" href="/img/icons/favicon-512.png" />
           <link rel="icon" href="/img/icons/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
           <meta name="theme-color" content="#0073e6" />
         </Head>
         <body>
