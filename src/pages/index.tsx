05d876c54bbb06f496655c24c56c91c6b847bff2
Willian Dias Tamagi <will.tamagi@gmail.com>
2021-01-03 17:30:59 -0300

    Fix null possibility

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,12 +5,36 @@ const MainPage: React.FC = () => {
   return (
     <div
       css={css`
+        display: grid;
+        grid-template-columns:
+          1fr
+          min(65ch, 100%)
+          1fr;
         width: 100%;
         height: 100%;
-        background-color: red;
+        padding: 3rem 0;
+
+        & > * {
+          grid-column: 2;
+        }
       `}
     >
-      TESTE
+      <div
+        css={css`
+          display: flex;
+          flex-direction: column;
+          font-size: 40px;
+        `}
+      >
+        <span>Hello there!</span>
+        <span>
+          {`I'm `}
+          <b>Willian</b>.
+        </span>
+        <span>
+          <b>Senior</b> Front End <b>Engineer</b>.
+        </span>
+      </div>
     </div>
   );
 };
--- src/pages/resume/components/ExperienceItem.tsx
+++ src/pages/resume/components/ExperienceItem.tsx
@@ -20,22 +20,22 @@ const ExperienceItem: React.FC<ExperienceItemProps> = ({ item }) => (
         font-weight: 600;
       `}
     >
-      {item.position_title}
+      {item?.position_title}
     </span>
     <span
       css={css`
         font-size: 12px;
         font-style: italic;
       `}
-    >{`${item.organization_name} | ${item.start_date} - ${
-      item.is_current ? 'Current' : item.end_date
+    >{`${item?.organization_name} | ${item?.start_date} - ${
+      item?.is_current ? 'Current' : item?.end_date
     }`}</span>
     <span
       css={css`
         margin-top: 0.5rem;
       `}
     >
-      <RichText render={item.position_description} />
+      <RichText render={item?.position_description} />
     </span>
   </div>
 );
