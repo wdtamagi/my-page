8c6a1da1c2eaa80837d2b0247329c6c244a0fca7
Willian Dias Tamagi <will.tamagi@gmail.com>
2021-01-03 19:30:33 -0300

    Add animation wth react spring

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
 import { css } from '@emotion/react';
+import React, { useRef } from 'react';
+import { useSpring, animated, useChain } from 'react-spring';
 
 const MainPage: React.FC = () => {
+  const fristRef = useRef(null);
+  const secondRef = useRef(null);
+  const thirdRef = useRef(null);
+
+  const fristProp = useSpring({
+    config: { duration: 800 },
+    from: { opacity: 0 },
+    ref: fristRef,
+    to: { opacity: 1 },
+  });
+  const secondProp = useSpring({
+    config: { duration: 800 },
+    from: { opacity: 0 },
+    ref: secondRef,
+    to: { opacity: 1 },
+  });
+  const thirdProp = useSpring({
+    config: { duration: 800 },
+    from: { opacity: 0 },
+    ref: thirdRef,
+    to: { opacity: 1 },
+  });
+
+  useChain([fristRef, secondRef, thirdRef]);
+
   return (
     <div
       css={css`
@@ -26,14 +52,14 @@ const MainPage: React.FC = () => {
           font-size: 40px;
         `}
       >
-        <span>Hello there!</span>
-        <span>
+        <animated.span style={fristProp}>Hello there!</animated.span>
+        <animated.span style={secondProp}>
           {`I'm `}
           <b>Willian</b>.
-        </span>
-        <span>
+        </animated.span>
+        <animated.span style={thirdProp}>
           <b>Senior</b> Front End <b>Engineer</b>.
-        </span>
+        </animated.span>
       </div>
     </div>
   );
