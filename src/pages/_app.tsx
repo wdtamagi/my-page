6aa2a3a246eda5726aa700d1005b76143dbfed77
Willian Dias Tamagi <will.tamagi@gmail.com>
2021-01-10 21:46:01 -0300

    Adjust the page title and ico

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -20,6 +20,10 @@ const MyApp = ({ Component, pageProps }: AppPropsType): JSX.Element => {
           href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
           rel="stylesheet"
         />
+        <link rel="icon" sizes="192x192" href="/img/icons/favicon-192.png" />
+        <link rel="apple-touch-icon" href="/img/icons/favicon-512.png" />
+        <link rel="icon" href="/img/icons/favicon.ico" />
+        <title>Will</title>
       </Head>
 
       <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -19,18 +19,7 @@ class MyDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang="en">
-        <Head>
-          <link rel="manifest" href="/manifest.json" />
-          <link rel="icon" sizes="192x192" href="/img/icons/favicon-192.png" />
-          <link rel="apple-touch-icon" href="/img/icons/favicon-512.png" />
-          <link rel="icon" href="/img/icons/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
-            rel="stylesheet"
-          />
-          <meta name="theme-color" content="#0073e6" />
-        </Head>
+        <Head></Head>
         <body>
           <Main />
           <NextScript />
